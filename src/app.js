26d167fd5758ecf730bdb4d1a19f1ce1d3bc5104
ashutosh sharma <ashusharmads.18@gmail.com>
2024-11-06 19:18:57 +0530

    add user and videomodel

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import cookieParser from 'cookie-parser'
 
 const app=express()
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+app.use(cors({ 
+    origin:process.env.CORS_ORIGIN,                       //origin from where my backend can take request
     credentials:true,
 }))
 
 app.use(express.json({limit:"16kb"}))                     //limit of json data
-app.use(express.urlencoded({extended:true,limit:"16kb"})) 
+app.use(express.urlencoded({extended:true,limit:"16kb"})) //url data
 app.use(express.static("public"))                         //to store public data like image
 app.use(cookieParser())                                   //My server can change user's broswer cookie through cookie parser
 
@@ -19,4 +19,4 @@ app.use(cookieParser())                                   //My server can change
 export {app}
 
 
-//app is an object of express use for configuration and middleware
+//app is an object of expres and its app.use() is used for configuration and middleware
