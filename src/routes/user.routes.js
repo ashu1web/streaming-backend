25d7b88ec152dccb9b79d664bb6cc6a50d06d99e
ashutosh sharma <ashusharmads.18@gmail.com>
2024-11-16 11:41:22 +0530

    Add comment like tweets playlist controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {loginUser, logoutUser, registerUser,refreshAcessToken} from "../controllers/user.controller.js";
+import {loginUser, logoutUser, registerUser,refreshAcessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +23,19 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAcessToken)
+router.route("/change-password").post(verifyJWT,
+changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,
+getCurrentUser)
+router.route("/update-account").patch(verifyJWT,
+updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),
+updateUserAvatar)
+router.route("/cover-Image").patch(verifyJWT,upload.single("coverImage"),
+updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT,
+getUserChannelProfile)
 
 export default router
