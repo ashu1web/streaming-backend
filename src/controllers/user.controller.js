f264ff8b6bfeb510eeede488cbf43cd6bdec06ce
ashutosh sharma <ashusharmads.18@gmail.com>
2024-11-17 21:36:00 +0530

    Changes in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -361,7 +361,7 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
     const {username}=req.params
 
     if(!username?.trim()){
-        throw new ApiError(400,"usernmae is missing")
+        throw new ApiError(400,"username is missing")
     }
 
     const channel=await User.aggregate([
