6536c03883f7a7fd2f766b34a85243643b133174
ashutosh sharma <ashusharmads.18@gmail.com>
2024-11-17 21:36:36 +0530

    Information about flow of tokens

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -26,3 +26,13 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
     }
     
 })
+
+
+/*"JWT is used to generate an access token.
+The private/secret key is stored securely on the server and is used to sign the access token.
+When the server receives the access token, it verifies it using the stored key to ensure its validity.
+If the token is valid, the user is granted access to the data. When the access token expires, 
+the client uses the refresh token (stored securely on the client side) to request a new access token without requiring the user to log in again."
+
+Cookies are a common and secure way to store both access tokens and refresh tokens, 
+provided the proper security flags (HttpOnly, Secure, SameSite) are used.*/
