c18a6fdd6b516f604debb1085f89e458dae7f58a
ashutosh sharma <ashusharmads.18@gmail.com>
2025-03-31 09:52:54 +0530

    Api error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,9 +1,5 @@
 class ApiError extends Error{
-    constructor(
-       statusCode,
-       message="Something went wrong",
-       errors=[],
-    ){
+    constructor(statusCode,message="Something went wrong",errors=[],){
         super(message)
         this.statusCode=statusCode
         this.data=null
